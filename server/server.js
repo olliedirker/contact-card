fa616a08142d21589ea7564aede3ee79accb426c
olliedirker <olliedirker@gmail.com>
2022-09-21 10:20:10 -0500

    Add to staging Lesson 2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
- 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,7 +7,7 @@ app.use(express.urlencoded({ extended:true}));
 app.use(express.json());
 app.use(express.static('../client/dist/'))
 
-require('./routes/htmlroutes')(app);
+require('./routes/htmlroutes.js')(app);
 
 app.listen(PORT, function(){
     console.log(`Now listening on port: ${PORT}`);
